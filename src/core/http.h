7b662769f95bb985bf9af992d7215896cd98435a
HackingPheasant <HackingPheasant@protonmail.com>
2023-09-11 22:31:14 +1000

    deps: Remove cpr dependency
    When the time comes that I will be making http and related requests
    again, I will just use curl directly :)

--- src/core/http.h
+++ src/core/http.h
@@ -1,14 +1,7 @@
 #ifndef INCLUDE_HTTP_H
 #define INCLUDE_HTTP_H
 
-#include <cpr/cpr.h>
-
 namespace http {
-
-// what the function needs at minimum: url
-//  url, cookies (tbd)
-auto get(std::string) -> cpr::Response;
-auto post(std::string) -> cpr::Response;
 
 } // namespace http
 

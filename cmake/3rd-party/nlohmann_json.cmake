c7b16ef761a5f7a8c94fc08c42cf3d1ddee0dff5
HackingPheasant <HackingPheasant@protonmail.com>
2023-11-13 23:52:41 +1100

    CMake: Fix build breakage, improve CMake workflow

--- cmake/3rd-party/nlohmann_json.cmake
+++ cmake/3rd-party/nlohmann_json.cmake
@@ -12,15 +12,21 @@ if (NOT DEFINED JSON_Diagnostics)
     set(JSON_Diagnostics ON)
 endif()
 
+# Note (2023-11-13): Swapped to offical repo again and pulling via a commit tag 
+# anytime after
+# https://github.com/nlohmann/json/commit/fac07e22c5d7dd0423ccf31c02db5603d27e6556
+# so we don't face CMake deprecation warings while using CMake 2.7 or above
+
 # Note: Using alternative url as suggested by:
 # https://github.com/nlohmann/json#embedded-fetchcontent
 # For a (much) smaller repo size/download
 include(FetchContent)
 FetchContent_Declare(
     nlohmann_json
-    # GIT_REPOSITORY https://github.com/nlohmann/json.git
-    GIT_REPOSITORY https://github.com/ArthurSonzogni/nlohmann_json_cmake_fetchcontent.git
-    GIT_TAG v3.11.2
+    GIT_REPOSITORY https://github.com/nlohmann/json.git
+    # GIT_REPOSITORY https://github.com/ArthurSonzogni/nlohmann_json_cmake_fetchcontent.git
+    GIT_TAG 6eab7a2b187b10b2494e39c1961750bfd1bda500
+    GIT_SHALLOW TRUE
     FIND_PACKAGE_ARGS
     )
 
